d29bc9bf5eea2d9ef264f846d511b6d0145540c7
zalakuldip740 <zalakuldipsinh740@gmail.com>
2021-11-29 19:14:40 +0530

    sqlite task

--- flutter training/sqlite_assesment_task/lib/features/audit_entity_feature/data/data_sources/audit_entity_data_source.dart
+++
@@ -1,8 +0,0 @@
-import 'package:sqlite_assesment_task/features/audit_entity_feature/domain/entities/audit_entity.dart';
-
-abstract class AuditEntityDataSource {
-  Future getAuditEntity();
-  Future<void> updateAuditEntity(AuditEntityData2 auditEntityData);
-  Future<void> deleteAuditEntity(AuditEntityData2 auditEntityData);
-  Future<void> insertAuditEntity(AuditEntityData2 auditEntityData);
-}
